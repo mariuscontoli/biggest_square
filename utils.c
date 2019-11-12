6183e047edf5c46e7fd3102dad9f84c9a297144b
Marius <marius.contoli-legay@epitech.eu>
2019-11-12 19:18:24 +0100

    first push utils source file functions

---
+++ utils.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int my_putstr(char const *str)
+{
+    int i = 0;
+    while (str[i] != '\0') {
+        my_putchar(str[i]);
+        i++;
+    }
+}
