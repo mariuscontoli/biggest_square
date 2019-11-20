7051d06a5d2a650837e1da2fbe54f3c1a14dc536
Marius <marius.contoli-legay@epitech.eu>
2019-11-20 18:34:21 +0100

    big push, im adding the function to convert my char * to int *, it output numbers who represents the size of my square at x index; and function to print (just testing) the int array, plus a function who find the biggest int in the array; next to do : display the square in a char * at the biggest int index

--- main.c
+++ main.c
@@ -10,11 +10,10 @@
 int skip_first_line(map_t *map)
 {
     int i = 0;
-    int k = 0;
     int count = 0;
-    for (; map->buffer[i]; i++)
+    for (; map->sv_map[i]; i++)
     {
-        if (map->buffer[i] == '\n')
+        if (map->sv_map[i] == '\n')
             break;
     }
     return (i + 1);
@@ -23,48 +22,95 @@ int skip_first_line(map_t *map)
 int find_rows(map_t *map)
 {
     int count = 0;
-    for (; map->buffer[map->rows] != '\n'; map->rows++) {
+    int i = map->skip;
+    for (; map->sv_map[i] != '\n'; i++) {
         count++;
     }
     return (count);
 }
 
-int *map_replaced(int *buffer, int i, int rows)
+int *map_replaced(map_t *map)
 {
-    int j = i;
-    while (buffer[i] != '\n') {
-        if (buffer[i] == 46) {
-            buffer[i] = 1;
+    while (map->sv_map[map->skip] != '\0') {
+        if (map->sv_map[map->skip] == '\n') {
+            map->buffer[map->index_buf] = -1;
+            map->skip++;
+            map->index_buf++;
         }
-        i++;
-    }
-    while (buffer[i] != '\0') {
-        if (buffer[i] == '\n') {
-            i++;
-            if (buffer[i] == 46) {
-                buffer[i] = 1;
-            }
+        if (map->sv_map[map->skip] == '.') {
+            map->buffer[map->index_buf] = 1;
+            map->skip++;
+            map->index_buf++;
         }
-        i++;
+        if (map->sv_map[map->skip] == 'o') {
+            map->buffer[map->index_buf] = 0;
+            map->skip++;
+            map->index_buf++;
+        }
     }
-    return (buffer);
+    map->buffer[map->index_buf] = -2;
+    map->index_buf = 0;
+    return (map->buffer);
 }
 
-int *map_with_2(int *buffer, int i, int rows)
+int find_smallest(map_t *map)
 {
-    int j;
-    j = i + rows + 1;
-    while (buffer[j] != '\0') {
-        if (buffer[j] == 46) {
-            if (buffer[j - 1] == 1 && buffer[j - (rows + 1)] == 1 && buffer[j - (rows + 2)] == 1) {
-                buffer[j] = 2;
-            } else {
-                buffer[j] = 1;
+    int a = map->buffer[map->index_buf - map->left];
+    int b = map->buffer[map->index_buf - map->top];
+    int c = map->buffer[map->index_buf - map->top_left];
+    if (a <= b && a <= c) 
+        return (a);
+    else if (b <= a && b <= c)
+        return (b);
+    else 
+        return (c);
+}
+
+int find_biggest(map_t *map)
+{
+    int i = 0;
+    int highest = 0;
+    while (map->buffer[i] != -2) {
+        if (map->buffer[i] > highest) {
+            highest = map->buffer[i];
+        }
+        i++;
+    }
+    return (highest);
+}
+
+int *map_with_2(map_t *map)
+{
+    map->left = 1;
+    map->top = (map->rows + 1);
+    map->top_left = (map->rows + 2);
+    map->index_buf = (map->index_buf + map->rows);
+    while (map->buffer[map->index_buf] != -2) {
+        if (map->buffer[map->index_buf] == 1) {
+            if (map->buffer[map->index_buf - map->left] > 0 &&
+            map->buffer[map->index_buf - map->top] > 0 && 
+            map->buffer[map->index_buf - map->top_left] > 0) {
+                map->buffer[map->index_buf] = (find_smallest(map) + 1);
             }  
         }
-        j++; 
+        map->index_buf++; 
     }
-    return (buffer);
+    return (map->buffer);
+}
+
+int print_buffer(map_t *map)
+{
+    int i = 0;
+    while (map->buffer[i] >= -1) {
+        if (map->buffer[i] == -1 && map->buffer[i + 1] != -2) {
+            my_put_nbr(map->buffer[i]);
+            my_putchar('\n');
+            i += 1;
+        }
+        my_put_nbr(map->buffer[i]);
+        i += 1;
+    }
+    return (0);
 }
 
 void my_bsq(char *filepath)
@@ -72,17 +118,25 @@ void my_bsq(char *filepath)
     map_t *map = malloc(sizeof(*map));
     struct stat off_t;
     int size2 = 0;
-    int test =0;
+    map->index_buf = 0;
     int fd = open(filepath, O_RDONLY);
     size2 = stat(filepath, &off_t);
     if (fd == -1 || size2 < 0) {
         exit (84);
     }
-    map->buffer = malloc(sizeof(char) * (off_t.st_size + 1));
-    read(fd, map->buffer, off_t.st_size);
-    map->rows = skip_first_line(map);
-    my_putstr(map->buffer);
-    free(map->buffer);
+    map->sv_map = malloc(sizeof(char) * (off_t.st_size + 1));
+    map->buffer = malloc(sizeof(int) * (off_t.st_size + 1));
+    read(fd, map->sv_map, off_t.st_size);
+    map->skip = skip_first_line(map);
+    map->rows = find_rows(map);
+    /* map rows is equal to 100 here
+    and map skip is equal to 4 */
+    map->buffer = map_replaced(map);
+    map->buffer = map_with_2(map);
+    map->biggest = find_biggest(map);
+    //my_putstr(map->sv_map);
+    print_buffer(map);
+    free(map->sv_map);
     free(map);
 }
 
