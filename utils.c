eda14b935ec8acfe5664a6d817be60cbcfd78fb9
Marius <marius.contoli-legay@epitech.eu>
2019-11-20 18:37:43 +0100

    check commit

--- utils.c
+++ utils.c
@@ -13,3 +13,22 @@ int my_putstr(char const *str)
         i++;
     }
 }
+
+int my_put_nbr(int nb)
+{
+    int modulo;
+    if (nb < 0) {
+        my_putchar('-');
+        nb = nb * (-1);
+    }
+    if (nb >= 0) {
+        if (nb >= 10) {
+            modulo = (nb % 10);
+            nb = (nb - modulo) / 10;
+            my_put_nbr(nb);
+            my_putchar(48 + modulo);
+        }
+        else
+            my_putchar(48 + nb % 10);
+    }
+}
