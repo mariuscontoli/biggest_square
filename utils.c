004f232f04e9668059cf6085d3b4e38938eba690
Marius <marius.contoli-legay@epitech.eu>
2019-11-21 10:24:17 +0100

    dd

--- utils.c
+++ utils.c
@@ -1,17 +1,8 @@
-#include "my_bsq.h"
+#include "include/my_bsq.h"
 
 void my_putchar(char c)
 {
     write(1, &c, 1);
-}
-
-int my_putstr(char const *str)
-{
-    int i = 0;
-    while (str[i] != '\0') {
-        my_putchar(str[i]);
-        i++;
-    }
 }
 
 int my_put_nbr(int nb)
